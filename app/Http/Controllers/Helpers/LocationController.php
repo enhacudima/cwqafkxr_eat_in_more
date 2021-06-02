ab10f0ca475bb6098e9764d4cb4a6ae53fc9e996
Emídio Carlos Nhacudima <kalibredj@outlook.com>
2021-06-02 19:23:54 +0200

    fix

--- app/Http/Controllers/Helpers/LocationController.php
+++ app/Http/Controllers/Helpers/LocationController.php
@@ -38,7 +38,6 @@ class LocationController extends Controller
     public function create()
     {
         $states = CountryStates::where('phone',$this->location_id)->first();
-        dd($states->id);
 
         UserL::updateOrCreate(
 
